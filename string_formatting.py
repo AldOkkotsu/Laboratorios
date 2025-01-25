102b62cfce14cca48805653e60e9c9b89b87d6b2
AldOkkotsu <aldoreza23@gmail.com>
2025-01-24 23:01:43 -0600

    Delete string_formatting.py

--- string_formatting.py
+++
@@ -1,8 +0,0 @@
-score = 8
-
-# str.format
-message = 'Si quiero aprobar mis materias, la calificación meinima es {}'.format(score)
-print (message)
-# F-String
-mensaje = f'Si quiero aprobar mis naterias, la calificación minima es {score}'
-print (mensaje)
