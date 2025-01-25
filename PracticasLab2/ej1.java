c56c5bdcd931b84bd81a742dfad9878937ebd0bf
AldOkkotsu <aldoreza23@gmail.com>
2025-01-24 23:15:17 -0600

    Add files via upload

---
+++ PracticasLab2/ej1.java
@@ -0,0 +1,23 @@
+package edu.utvt;
+import java.util.Scanner;
+
+public class ej1 {
+
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String message = "hola %s, tu edad es %s";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ingrese nombre: ");
+        name = sc.nextLine();
+
+        System.out.print("ingrese edad: ");
+        age = Integer.parseInt(sc.nextLine());
+
+        sc.close();
+        String finalMessage = String.format(message, name, age);
+        System.out.print(finalMessage);
+    }
+}
